92b5e3823c83222ca3370f65608fdb88ad1447f4
Henri Pietila <henrismail@gmail.com>
2017-04-29 19:08:26 -0700

    more testing

--- client/js/test/dom-util.test.js
+++ client/js/test/dom-util.test.js
@@ -1,20 +1,55 @@
-const { removeChildren } = require('../dom-util');
+const { createTR,
+        createTH,
+        createTD,
+        removeChildren } = require('../dom-util');
 
 describe('dom-util', () => {
-  it('removes one child', () => {
-    //set up intial state
-    const parent = document.createElement('DIV');
-    const child = document.createElement('STRONG');
-    parent.appendChild(child);
 
-    // inspect intial state
-    expect(parent.childNodes.length).toBe(1);
-    expect(parent.childNodes[0]).toBe(child);
+  describe('DOM creation functions', () => {
+    describe('createTH', () => {
+      it('produces valid TH element', () => {
+        const el = createTH();
+        expect(el.tagName).toBe('TH');
+      });
+      it('sets the text of the TH', () => {
+        const text = "Oh tha's just great! well game over man!";
+        const el = createTH(text);
+        expect(el.textContent).toBe(text);
+      });
+    });
 
-    //execute code under test
-    removeChildren(parent);
+    describe('createTD', () => {
+      it('produces valid TD element', () => {
+        const el = createTD();
+        expect(el.tagName).toBe('TD');
+      });
+    });
 
-    //insepct resulting state
-    expect(parent.childNodes.length).toBe(0);
+    describe('createTR', () => {
+      it('produces valid TR element', () => {
+        const el = createTR();
+        expect(el.tagName).toBe('TR');
+      });
+    });
   });
+
+  describe('removeChilderen()', () => {
+    it('removes one child', () => {
+      //set up intial state
+      const parent = document.createElement('DIV');
+      const child = document.createElement('STRONG');
+      parent.appendChild(child);
+
+      // inspect intial state
+      expect(parent.childNodes.length).toBe(1);
+      expect(parent.childNodes[0]).toBe(child);
+
+      //execute code under test
+      removeChildren(parent);
+
+      //insepct resulting state
+      expect(parent.childNodes.length).toBe(0);
+    });
+  });
+
 });
